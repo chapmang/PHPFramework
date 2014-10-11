0606533779a765d8ddbfa8898ea2acee7fd952a1
Geoff Chapman <geoff.chapman@mac.com>
2014-10-11 23:37:49 +0100

    Testing
    Building methods to allow for cache testing

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,17 +1,25 @@
 <?php
 
 use Shared\Controller as Controller;
+use Framework\Registry as Registry;
 
 class Home extends Controller {
 
 
     public function getIndex() {
-        
+        $cache = Registry::get('cache');
+        $cache->set('test', "Geoff");
+        $cache->set('test1', "Chapman");
+        $cache->deleteAll();
+        $name = $cache->get('test');
+        var_dump($name);
     }
 
 
 
     public function getTest(){
+
+    	$cache = Registry::get('cache');
     	if ($this->_ajax()) {
     	$test = array("Chapman");
     	echo json_encode($test);
