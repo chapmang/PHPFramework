13e8087e2e2fc2a5b8855c11a9a1c59196dc7352
Geoff Chapman <geoff.chapman@mac.com>
2014-10-12 13:28:29 +0100

    Add static 'logFile' property

--- framework/log.php
+++ framework/log.php
@@ -10,6 +10,8 @@ namespace Framework {
 	 */
 	class Log {
 
+		protected static $_logPath;
+
 		private function __construct() {
 			// do nothing
 		}
@@ -27,16 +29,16 @@ namespace Framework {
 
 			// Fetch log location from config file 
 			if (Configuration::get('error.logPath')) {
-				$logFile = Configuration::get('error.logPath');
+				self::$_logPath = Configuration::get('error.logPath');
 			} else {
 				// Default framework log location 
-				$logFile = path('storage') . "logs/errors/" . date("Y-m-d") . ".log";
+				self::$_logPath = path('storage') . "logs/errors/" . date("Y-m-d") . ".log";
 			}
 
 			// Write the entry to the log file
 			$message = $e->getMessage() . ' in ' . $e->getFile() . ' on line ' . $e->getLine();
 			
-			static::write('error', $logFile, $message);
+			static::write('error', $message);
 		}
 
 		/**
@@ -46,13 +48,13 @@ namespace Framework {
 		 * @param string $message Formated log message
 		 * @return void           	
 		 */
-		public static function write($type, $logFile, $message) {
+		public static function write($type, $message) {
 
 			// Write the entry to the log file
 			$type = strtoupper($type);
 			$template = "[".date('Y-m-d H:i:s')."] %s - %s".PHP_EOL;
 			$logEntry = sprintf($template, $type, $message);
-			file_put_contents($logFile, $logEntry, FILE_APPEND);
+			file_put_contents(self::$_logPath, $logEntry, FILE_APPEND);
 
 		}
 
